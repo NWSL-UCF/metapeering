28844891454bf53567400572abb3bb8bac342d0c
prasun <prasun@knights.ucf.edu>
2020-04-22 03:05:39 +0000

    Fixed ISP names

--- app/forms.py
+++ app/forms.py
@@ -30,34 +30,34 @@ class PeeringQueryForm(Form):
     
 	options_grouped_asn = [
 				('Access ISPs', (
-						(11492, 'cableone'), 
-						(209, 'centurylink'),
-						(7843, 'charter'), 
-						(7922, 'comcast'), 
-						(22773, 'cox'), 
-						(4181, 'tds'), 
-						(7029, 'windstream')
+						(11492, 'Cable ONE, Inc.'), 
+						(209, 'CenturyLink Communications, LLC'),
+						(7843, 'Charter Communications Inc.'), 
+						(7922, 'Comcast Cable Communications, LLC'), 
+						(22773, 'Cox Communications Inc.'), 
+						(4181, 'TDS Telecom'), 
+						(7029, 'Windstream Communications LLC')
 						)
 			  	),
                 ('Content ISPs', (
-						(20940, 'akamai'), 
-						(16509, 'amazon'), 
-						(62955, 'ebay'), 
-						(32934, 'facebook'), 
-						(15169, 'google'), 
-						(8075, 'microsoft'), 
-						(2906, 'netflix')
+						(20940, 'Akamai International B.V.'), 
+						(16509, 'Amazon'), 
+						(62955, 'Ebay'), 
+						(32934, 'Facebook'), 
+						(15169, 'Google'), 
+						(8075, 'Microsoft Corporation'), 
+						(2906, 'Netflix')
 						)
 				),
                	('Transit ISPs', (
-						(23520, 'columbus'), 
-						(174, 'cogent'), 
-						(6939, 'he'), 
-						(2914, 'ntt'), 
-						(3491, 'pccw'), 
-						(1239, 'sprint'), 
-						(701, 'verizon'), 
-						(6461, 'zayo')
+						(23520, 'Columbus Networks USA, Inc.'), 
+						(174, 'Cogent Communications'), 
+						(6939, 'Hurricane Electric LLC'), 
+						(2914, 'NTT America, Inc.'), 
+						(3491, 'PCCW Global Inc.'), 
+						(1239, 'Sprint'), 
+						(701, 'Verizon Communications, Inc.'), 
+						(6461, 'Zayo (Abovenet Communications Inc.)')
 						)
 				)
 			]
