c5742ccdc9ec6c63a20269a7e677787eb5489ec2
mark-durbin-ucf <mark.durbin@ucf.edu>
2026-06-02 14:29:40 -0400

    Add prebuild hook to install geospatial system libraries via dnf
    - Fiona, geopandas, pyproj, Shapely require GDAL, GEOS, PROJ C libraries
    - These are not available by default on AL2023 and must be installed before pip

--- .platform/hooks/prebuild/00_hooks_prebuild.sh
+++ .platform/hooks/prebuild/00_hooks_prebuild.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -e
+
+# Install system-level geospatial libraries required by Fiona, geopandas, pyproj, Shapely
+dnf install -y gdal gdal-libs gdal-devel geos geos-devel proj proj-devel
 
 echo "00_hooks_prebuild.sh was run"
